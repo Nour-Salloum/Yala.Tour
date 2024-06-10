81a94ff5ea36f5b3c2dc5b6a1a270c55e7627fa6
Zahraaa4 <zha166@usal.edu.lb>
2024-06-11 01:14:00 +0300

    Design Till now

--- app/src/main/java/com/example/yalatour/DetailsActivity/DetailActivity.java
+++ app/src/main/java/com/example/yalatour/DetailsActivity/DetailActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,7 +20,8 @@ import com.example.yalatour.R;
 public class DetailActivity extends AppCompatActivity {
     TextView detailDesc, detailTitle;
     ImageView detailImage;
-    Button showHotelsButton,showHospitalsButton,showRestaurantsButton,showTourismPlacesButton;
+    Button showTourismPlacesButton;
+    ImageButton showHotelsButton,showHospitalsButton,showRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
